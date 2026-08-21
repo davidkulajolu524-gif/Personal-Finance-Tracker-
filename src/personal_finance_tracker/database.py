79b38df82329c82f7e6d1adc2003e0c3aae67f30
davidkulajolu524-gif <davidkulajolu524@gmail.com>
2026-08-21 08:57:56 -0300

    Use PostgreSQL on hosted environment

--- src/personal_finance_tracker/database.py
+++ src/personal_finance_tracker/database.py
@@ -1,18 +1,31 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
-DATABASE_URL = "sqlite:///./finance.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./finance.db",
+)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={
+            "check_same_thread": False,
+        },
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+    )
+
 
 SessionLocal = sessionmaker(
     bind=engine,
@@ -20,5 +33,8 @@ SessionLocal = sessionmaker(
     autocommit=False,
 )
 
+
 def create_tables():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(
+        bind=engine,
+    )
